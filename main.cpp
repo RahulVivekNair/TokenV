a1a66f373c62a82144994aeca894bf0b36202921
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2023-09-08 15:52:53 +0530

    Added custom word file support

--- main.cpp
+++ main.cpp
@@ -3,31 +3,74 @@
 #include <vector>
 #include <thread>
 #include <chrono>
+#include <fstream>
+#include <filesystem>
 using namespace std;
+
+vector<string> loadWordsFromFile(const string &filename)
+{
+    vector<string> words;
+    ifstream file(filename);
+    string word;
+
+    while (file >> word)
+    {
+        words.push_back(word);
+    }
+
+    return words;
+}
 int main(int argc, char **argv)
 {
-    if (argc != 3 || string(argv[1]) != "-t")
+    if (argc < 3)
     {
-        std::cerr << "Usage: main.exe -t <number_of_tokens>" << endl;
+        cerr << "Usage: main.exe -t <number_of_tokens> [-f <filename>]" << endl;
         return 1;
     }
-    double numTokens = atof(argv[2]);
+
+    double numTokens = 0.0;
+    string filename;
+    vector<string> words;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        if (string(argv[i]) == "-t" && i + 1 < argc)
+        {
+            numTokens = atof(argv[i + 1]);
+        }
+        else if (string(argv[i]) == "-f" && i + 1 < argc)
+        {
+            filename = argv[i + 1];
+        }
+    }
+
     if (numTokens <= 0)
     {
-        std::cerr << "Invalid number of tokens" << endl;
+        cerr << "Invalid number of tokens" << endl;
         return 1;
     }
-    vector<string> words = {"Hello", "world", "This", "is", "a", "sample", "output", "from", "the", "program.", "Let's", "see", "how", "it", "works.", "Let's", "see", "how", "fast", "it", "goes."};
-    cout << "Hello World!" << endl;
+
+    words = {"Hello", "world", "This", "is", "a", "sample", "output", "from", "the", "program.", "Let's", "see", "how", "it", "works.", "Let's", "see", "how", "fast", "it", "goes."};
+
+    if (!filename.empty() && filesystem::exists(filename) && filesystem::is_regular_file(filename))
+    {
+        words = loadWordsFromFile(filename);
+    }
+
+    if (words.empty())
+    {
+        cerr << "Error: Failed to load words from the file or no default words provided." << endl;
+        return 1;
+    }
     int delayDuration = static_cast<int>((1.0 / numTokens) * 1000000);
+
     while (true)
     {
-        for (auto token : words)
+        for (const auto &token : words)
         {
             cout << token << " ";
             this_thread::sleep_for(chrono::microseconds(delayDuration));
         }
-        cout << endl;
     }
 
     return 0;
