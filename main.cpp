072146cf8c85ec5e8d911a3c0cb9bc8a63b6b4d5
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2023-09-08 14:30:06 +0530

    Basic working code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
 using namespace std;
 int main(int argc, char **argv)
 {
@@ -14,6 +17,18 @@ int main(int argc, char **argv)
         std::cerr << "Invalid number of tokens" << endl;
         return 1;
     }
+    vector<string> words = {"Hello", "world", "This", "is", "a", "sample", "output", "from", "the", "program.", "Let's", "see", "how", "it", "works.", "Let's", "see", "how", "fast", "it", "goes."};
     cout << "Hello World!" << endl;
+    int delayDuration = static_cast<int>((1.0 / numTokens) * 1000000);
+    while (true)
+    {
+        for (auto token : words)
+        {
+            cout << token << " ";
+            this_thread::sleep_for(chrono::microseconds(delayDuration));
+        }
+        cout << endl;
+    }
+
     return 0;
 }
